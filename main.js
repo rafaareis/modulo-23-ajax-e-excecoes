b973b2fc2b8beee17f02c373b4aa4a1bee7e6e57
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-06-11 15:44:50 -0300

    Commit 11062023-1544: Aula 4

--- main.js
+++ main.js
@@ -18,19 +18,39 @@ $(document).ready(function() {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
         
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const complemento = resposta.complemento;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${complemento}, ${bairro} - ${cidade} - ${estado}`
-            $('#endereco').val(endereco)
-            setTimeout(function() {
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            }, 500)
+        // $.ajax(endpoint).done(function(resposta) {
+        //     const logradouro = resposta.logradouro;
+        //     const complemento = resposta.complemento;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${complemento}, ${bairro} - ${cidade} - ${estado}`
+        //     $('#endereco').val(endereco)
+        //     setTimeout(function() {
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 500)
 
-        })
+        // })
+
+        fetch(endpoint)
+            .then(function(resposta) {
+                return resposta.json();
+            }).then(function(json) {
+                const logradouro = json.logradouro;
+                const complemento = json.complemento;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
+                const endereco = `${logradouro}, ${complemento}, ${bairro} - ${cidade} - ${estado}`
+
+                $('#endereco').val(endereco);
+
+                setTimeout(function() {
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+                }, 500)
+                
+            })
     })
 })
