48f3ac9ba4d1b8d92f0c394aeb2916051362e158
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-06-12 10:12:14 -0300

    Commit 12062023-1012: Aula 5

--- main.js
+++ main.js
@@ -36,7 +36,8 @@ $(document).ready(function() {
         fetch(endpoint)
             .then(function(resposta) {
                 return resposta.json();
-            }).then(function(json) {
+            })
+            .then(function(json) {
                 const logradouro = json.logradouro;
                 const complemento = json.complemento;
                 const bairro = json.bairro;
@@ -45,12 +46,31 @@ $(document).ready(function() {
                 const endereco = `${logradouro}, ${complemento}, ${bairro} - ${cidade} - ${estado}`
 
                 $('#endereco').val(endereco);
-
+            })
+            .catch(function(erro) {
+                alert("Ocorreu um erro ao buscar o endereço. Tente novamente mais tarde.")
+            })
+            .finally(function() {
                 setTimeout(function() {
                     $(botao).find('i').removeClass('d-none');
                     $(botao).find('span').addClass('d-none');
-                }, 500)
-                
+                }, 500)   
             })
+    })
+
+    $('#formulario-pedido').submit(function(evento) {
+        evento.preventDefault();
+
+        if($('#nome').val().length == 0) {
+            throw new Error ('Digite o nome');
+        }
+
+        if($('#sobrenome').val().length == 0) {
+            throw new Error ('Digite o sobrenome');
+        }
+
+        if($('#email').val().length == 0) {
+            throw new Error ('Digite o email');
+        }
     })
 })
